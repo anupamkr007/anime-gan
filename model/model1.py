8d33dfd308e7a4bcd517e0eb006f8ddeb4770af1
anupamkr007 <anupam.ismdhn@gmail.com>
2020-10-18 02:43:46 +0530

    model, parameter initialization

---
+++ model/model1.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+                nn.ConvTranspose2d(100, 512, 4, 1, 0, bias=False),
+                nn.BatchNorm2d(512),
+                nn.ReLU(True),
+
+                nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(256),
+                nn.ReLU(True),
+
+                nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(128),
+                nn.ReLU(True),
+
+                nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(64),
+                nn.ReLU(True),
+
+                nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+                nn.Tanh()
+                )
+
+        def forward(self, input):
+            return self.main(input)
+
+
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+                nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+                nn.LeakyReLU(0.2, inplace=True),
+
+                nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(128),
+                nn.ReLU(True),
+
+                nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(256),
+                nn.LeakyReLU(0.2, inplace=True),
+
+                nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(64),
+                nn.LeakyReLU(0.2, inplace=True),
+
+                nn.Conv2d(512, 1, 4, 1, 0, bias=False),
+                nn.Sigmoid()
+                )
+
+        def forward(self, input):
+            return self.main(input)
